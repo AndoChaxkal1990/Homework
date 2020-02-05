80e8456f611da840ca6588b2d1d0fc782eecb3d8
AndoChaxkal1990 <60383016+AndoChaxkal1990@users.noreply.github.com>
2020-02-05 15:12:12 +0400

    Delete homework1(5).js

--- homework1(5).js
+++
@@ -1,7 +0,0 @@
-let a=+prompt('asaceq arajin tiv@');
-let b=+prompt('asaceq erkrord tiv@');
-let c=+prompt('asaceq errord tiv@');
-let d=+prompt('asaceq chorrord tiv@');
-let e=+prompt('asaceq hingerord tiv@');
-alert((a+b+c+d+e)/5)
-
