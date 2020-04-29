a74949c070127b3e8a2cd8a4f6d981ddbfd48141
Ando Harutyunyan <60383016+AndoHarutyunyan@users.noreply.github.com>
2020-04-29 19:05:32 +0400

    Add files via upload

---
+++ 2 kurs homework 5.js
@@ -0,0 +1,45 @@
+//  Given a sorted array and an element from that array. Find the index of that
+//  element using binary search.
+
+function binarySearch(arr, n) {
+    let min = 0;
+    let max = arr.length - 1;
+    if(n<arr[min] ||n>arr[max]){
+        return -1;
+    }else{
+        for(let i=0; i<arr.length; ){
+            i = Math.floor((max + min)/2);
+            if(arr[i] === n){
+                return i;
+            }else if(max===min|| Math.abs(max-min===1)){
+                return -1;
+            }else if (arr[i] < n){
+                min = i +1;
+            }else if(arr[i]>n) {
+                max = i -1;
+            }
+        };
+    }
+}
+
+
+//. tarberak 2 rekursiayov
+
+function  binarySearch(arr,n,min=0,max=arr.length){
+    if(n<arr[min] ||n>arr[max-1]){
+        return false;
+    }else{
+        if(arr[min]===n){
+            return min
+        }else if(arr[min]<n){
+            min=Math.floor((min+1+max)/2);
+            if(arr[min]>n){
+                max=min
+                min=0
+            }
+            return binarySearch(arr, n , min,max);
+        }else{
+            return false;
+        }
+    }
+}
